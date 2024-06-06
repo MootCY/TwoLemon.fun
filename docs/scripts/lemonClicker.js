4e0aeeb12266498d099ef793c605e23db79efaa0
MootCY <martywelsh021@gmail.com>
2024-06-06 20:31:34 +1000

    trying to add new shop button

--- docs/scripts/lemonClicker.js
+++ docs/scripts/lemonClicker.js
@@ -1,22 +1,47 @@
 const counter = document.getElementById('counter');
 const lemon = document.getElementById('lemon');
 const clickMultiplier = document.getElementById('clickMultiplier');
+const lemomsPerSecondMultiplier = document.getElementById('lemomsPerSecondMultiplier');
 
 let lemons = 0;
-let lps = 1;
-let clickMultiplierPrice = 25;
+let lpc = 1;
+let lps = 0;
+let clickMultiplierPrice = 50;
+let lemomsPerSecondMultiplierPrice = 100;
+let lpsBought = false;
 
 lemon.addEventListener('click', function() {
-  lemons+= lps;
+  lemons+= lpc;
 
   counter.textContent = lemons + " Lemons";
 });
+
 clickMultiplier.addEventListener('click', function() {
   if (lemons >= clickMultiplierPrice) {
     lemons-= clickMultiplierPrice;
     counter.textContent = lemons + " Lemons";
-    clickMultiplierPrice+= 25;
+    clickMultiplierPrice+= clickMultiplierPrice;
     clickMultiplier.textContent = "+1 Lemons per click Cost: " + clickMultiplierPrice + " Lemons";
-    lps++;
+    lpc++;
   }
 });
+
+lemomsPerSecondMultiplier.addEventListener('click', function() {
+    if (lemons >= lemomsPerSecondMultiplierPrice){
+      lemons-= lemomsPerSecondMultiplierPrice;
+      lemomsPerSecondMultiplierPrice+= lemomsPerSecondMultiplierPrice;
+      lemomsPerSecondMultiplier.textContent = "+1 Lemons per second Cost: " + lemomsPerSecondMultiplierPrice + " Lemons";
+      lps++
+      if (lpsBought == false){
+        lpsBought = true;
+      }
+    }
+});
+
+function lpsUpdater(){
+  while(true){
+    lemons+= lps;
+    counter.textContent = lemons + " Lemons";
+    setTimeout(lpsUpdater, 100);
+  }
+}
