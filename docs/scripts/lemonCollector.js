5b52c0699173fe38d9c16ba0886a0ec7e36b065e
MootCY <martywelsh021@gmail.com>
2024-06-14 19:48:15 +1000

    added movement for lemon collector

--- docs/scripts/lemonCollector.js
+++ docs/scripts/lemonCollector.js
@@ -1,23 +1,30 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
 
-let playerX = 0;
-let playerY = 0;
+let playerX = canvas.width/2;
+let playerY = canvas.height/2;
+let playerXV = 0;
+let playerYV = 0;
 let playerSize = 50;
 
-function loop(){
-    draw()
-    update()
-    requestAnimationFrame(loop)
-}
+window.addEventListener("keydown", function(e){
+    if(e.code == "KeyW") playerYV = -5;
+    if(e.code == "KeyS") playerYV = 5;
+    if(e.code == "KeyD") playerXV = 5;
+    if(e.code == "KeyA") playerXV = -5;
+});
 
-function draw(){
-    ctx.fillRect(playerX, playerY, playerSize, playerSize);
-    ctx.clearRect(0, 0, 10000, 10000);
-}
+window.addEventListener("keyup", function(e){
+    if(e.code == "KeyW" || e.code == "KeyS") playerYV = 0;
+    if(e.code == "KeyA" || e.code == "KeyD") playerXV = 0;
+});
 
 function update(){
-
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    playerX += playerXV;
+    playerY += playerYV;
+    ctx.fillRect(playerX, playerY, playerSize, playerSize);
+    requestAnimationFrame(update);
 }
 
-loop()
+update();
