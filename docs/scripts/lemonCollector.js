4c40cab4d0b1d3f39ada009127534a532f983868
MootCY <martywelsh021@gmail.com>
2024-06-15 16:38:51 +1000

    Made the lemon and player actually appear

--- docs/scripts/lemonCollector.js
+++ docs/scripts/lemonCollector.js
@@ -7,12 +7,16 @@ let playerXV = 0;
 let playerYV = 0;
 let playerSize = 50;
 
-let lemonX = 0;
-let lemonY = 0;
+let lemonX = 300;
+let lemonY = 300;
 let lemonSize = 30;
 
 const lemonImg = new Image();
-lemonImg.src='images/Lemon.png';
+lemonImg.src = 'images/Lemon.png';
+
+lemonImg.onload = function() {
+    update();
+};
 
 window.addEventListener("keydown", function(e){
     if(e.code == "KeyW") playerYV = -5;
@@ -35,10 +39,10 @@ function update(){
 
     ctx.drawImage(lemonImg, lemonX, lemonY, lemonSize, lemonSize);
 
-    if(playerX + playerSize > lemonX && playerY + playerSize > lemonY && lemonX + lemonSize > playerX && lemonY + lemonSize > playerY){
+    if(playerX + playerSize > lemonX && playerY + playerSize > lemonY &&
+       lemonX + lemonSize > playerX && lemonY + lemonSize > playerY){
         console.log("Collision");
     }
+
     requestAnimationFrame(update);
 }
-
-update();
