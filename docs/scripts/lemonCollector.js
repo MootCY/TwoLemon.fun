ced8a5433c794cb55101f05607142c50d34c833e
MootCY <martywelsh021@gmail.com>
2024-06-15 19:20:44 +1000

    Chaged the position of the items in lemon collector

--- docs/scripts/lemonCollector.js
+++ docs/scripts/lemonCollector.js
@@ -1,14 +1,14 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
 
-let playerX = 400;
-let playerY = 400;
+let playerX = 0;
+let playerY = 0;
 let playerXV = 0;
 let playerYV = 0;
 let playerSize = 50;
 
-let lemonX = 300;
-let lemonY = 400;
+let lemonX = 70;
+let lemonY = 0;
 let lemonSize = 30;
 
 const lemonImg = new Image();
@@ -22,19 +22,15 @@ lemonImg.onload = function() {
 window.addEventListener("keydown", function(e){
     if(e.code == "KeyW"){
         playerYV = -5;
-        console.log("Key W pressed");
     }
     if(e.code == "KeyS"){
         playerYV = 5
-        console.log("Key S pressed");
     };
     if(e.code == "KeyD"){
         playerXV = 5;
-        console.log("Key D pressed");
     }
     if(e.code == "KeyA"){
         playerXV = -5
-        console.log("Key A pressed");
     };
 });
 
@@ -44,16 +40,11 @@ window.addEventListener("keyup", function(e){
 });
 
 function update(){
-    console.log("Updating...");
-
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    console.log("Cleared items");
 
     ctx.drawImage(lemonImg, lemonX, lemonY, lemonSize, lemonSize);
-    console.log("Drew lemon");
 
     ctx.fillRect(playerX, playerY, playerSize, playerSize);
-    console.log("Drew player");
 
     if(playerX + playerSize > lemonX && playerY + playerSize > lemonY && lemonX + lemonSize > playerX && lemonY + lemonSize > playerY){
         console.log("Collision detected");
@@ -62,6 +53,5 @@ function update(){
     playerX += playerXV;
     playerY += playerYV;
     
-    console.log("Updated successfully");
     requestAnimationFrame(update);
 }
