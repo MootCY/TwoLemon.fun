1c09a31944f85ede838fd3440b2e8b374ba108f5
MootCY <martywelsh021@gmail.com>
2024-06-15 19:34:57 +1000

    Made a score counter and made the lemon go to random places when you touch it

--- docs/scripts/lemonCollector.js
+++ docs/scripts/lemonCollector.js
@@ -1,37 +1,39 @@
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext("2d");
 
+let scoreCounter = document.getElementById('scoreCounter');
+let score = 0;
+
 let playerX = 0;
 let playerY = 0;
 let playerXV = 0;
 let playerYV = 0;
-let playerSize = 50;
+let playerSize = 30;
 
 let lemonX = 70;
 let lemonY = 0;
-let lemonSize = 30;
+let lemonSize = 50;
 
 const lemonImg = new Image();
 lemonImg.src="images/Lemon.png";
 
 lemonImg.onload = function() {
     update();
-    console.log("Image loaded");
 };
 
 window.addEventListener("keydown", function(e){
     if(e.code == "KeyW"){
-        playerYV = -5;
+        playerYV = -2;
     }
     if(e.code == "KeyS"){
-        playerYV = 5
-    };
+        playerYV = 2;
+    }
     if(e.code == "KeyD"){
-        playerXV = 5;
+        playerXV = 2;
     }
     if(e.code == "KeyA"){
-        playerXV = -5
-    };
+        playerXV = -2;
+    }
 });
 
 window.addEventListener("keyup", function(e){
@@ -47,7 +49,10 @@ function update(){
     ctx.fillRect(playerX, playerY, playerSize, playerSize);
 
     if(playerX + playerSize > lemonX && playerY + playerSize > lemonY && lemonX + lemonSize > playerX && lemonY + lemonSize > playerY){
-        console.log("Collision detected");
+        lemonX = Math.floor(Math.random()*800-lemonSize) + 1;
+        lemonY = Math.floor(Math.random()*600-lemonSize) + 1;
+        score++;
+        scoreCounter.textContent = score;
     }
 
     playerX += playerXV;
