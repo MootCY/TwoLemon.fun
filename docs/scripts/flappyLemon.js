1a0abe0b721176901a4349e94069095f791a9a61
MootCY <martywelsh021@gmail.com>
2024-09-19 14:44:56 +1000

    yay

--- docs/scripts/flappyLemon.js
+++ docs/scripts/flappyLemon.js
@@ -11,7 +11,7 @@ let playerSize = 80;
 let score = 0;
 let highScore = localStorage.getItem('highScore')||0;
 
-let gravity = 2;
+let gravity = 3;
 let jumpHeight = 60;
 
 let playerImg = new Image();
